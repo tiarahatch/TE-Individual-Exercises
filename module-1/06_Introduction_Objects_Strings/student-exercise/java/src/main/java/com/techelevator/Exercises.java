f9c16a4134ef078bbf460fdc5b78a4e4a29f6d0d
Tiara Hatch <tiara.hatch@outlook.com>
2020-01-21 23:08:54 -0500

    Final Submission

--- module-1/06_Introduction_Objects_Strings/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/06_Introduction_Objects_Strings/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -327,18 +327,22 @@ public class Exercises {
 	 doubleX("xxxxx") → true
 	 */
 	public boolean doubleX(String str) {
+		
 		for (int i=0; i < str.length() -1; i++) {
 			
-			if((str.charAt(i) == 'x') && (str.charAt(i + 1) == 'x')){
-				return true;
-			}
-			else {
+			if(str.charAt(i) == 'x') {
+			
+				if(str.charAt(i + 1) == 'x')
+					return true;
+				else 
 				return false;
 			}
-		return false;
 		}
-	}
-		
+			return false;
+			}
+
+	
+	
 
 
 	/*
@@ -348,8 +352,7 @@ public class Exercises {
 	 stringBits("Heeololeo") → "Hello"
 	 */
 	public String stringBits(String str) {
-		if ()
-		
+		return str;
 	}
 		 
 	/*
