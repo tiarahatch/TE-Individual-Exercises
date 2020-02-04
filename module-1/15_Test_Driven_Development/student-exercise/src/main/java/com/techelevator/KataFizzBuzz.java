50cf345d25a6d16cbf6b9a81160e3ec2b299d8f1
Tiara Hatch <tiara.hatch@outlook.com>
2020-02-03 23:51:03 -0500

    Updated

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,36 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	
+	public static String fizzBuzz(int num) {
+			
+			if(num % 3 == 0 && num % 5 == 0) {
+				return "FizzBuzz";
+			}
+			else if (Integer.toString(num).contains("3, 5") || (num % 3 == 0 && num % 5 == 0)) {
+				return "FizzBuzz";
+			}
+			else if (num % 3 == 0) {
+				return "Fizz";
+			}
+			else if (Integer.toString(num).contains("3") || num % 3 == 0) {
+				return "Fizz";
+			}
+			else if (num % 5 ==0) {
+				return "Buzz";
+			}
+			else if (Integer.toString(num).contains("5") || num % 5 == 0) {
+				return "Buzz";
+			}
+			else if (num >=1 || num <= 100) {
+				return Integer.toString(num);
+			}
+			else if (num < 1 || num > 100 ){
+				
+				return " ";
+			}
+		return Integer.toString(num);
+		
+	}
 
 }
--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,47 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	KataFizzBuzz FizzBuzz;
+	KataFizzBuzz Fizz;
+	KataFizzBuzz Buzz;
+	KataFizzBuzz num;
+	
+	@Before
+	public void setup() {
+		FizzBuzz = new KataFizzBuzz();
+		Fizz = new KataFizzBuzz();
+		Buzz = new KataFizzBuzz();
+		num = new KataFizzBuzz();
+	}
+	@Test
+	public void testWhenDivisibleBy3And5() {
+		Assert.assertEquals("FizzBuzz", num.fizzBuzz(15));
+	}
+	
+	@Test
+	public void testWhenDivisibleBy3() {
+		Assert.assertEquals("Fizz", num.fizzBuzz(3));
+	}
+	
+	@Test
+	public void testWhenDivisibleBy5() {
+		Assert.assertEquals("Buzz", num.fizzBuzz(5));
+	}
+	@Test
+	public void testWhenDivisibleBy5AndContains5() {
+		Assert.assertEquals("Buzz", num.fizzBuzz(5));
+		Assert.assertEquals("Buzz", num.fizzBuzz(51));
+	}
+	@Test
+	public void testWhenDivisibleBy3AndContains3() {
+		Assert.assertEquals("Fizz", num.fizzBuzz(3));
+		Assert.assertEquals("Fizz", num.fizzBuzz(13));
+	}
+	@Test
+	public void testWhenDivisibleBy5And3OrContains5nd3() {
+		Assert.assertEquals("FizzBuzz", num.fizzBuzz(35));
+		Assert.assertEquals("FizzBuzz", num.fizzBuzz(53));
+	}
 
 }
